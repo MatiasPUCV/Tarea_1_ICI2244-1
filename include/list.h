49cc9bd80c7aa1bfdfc35ddd07beb3bcbc06baf0
Estellar <142541831+MatiasPUCV@users.noreply.github.com>
2023-08-31 18:57:15 -0400

    Added "biblioteca.csv"

--- include/list.h
+++ include/list.h
@@ -4,20 +4,20 @@
 typedef struct List List;
 
 List* createList(void);
-void cleanList(List * list);
+void cleanList(List* list);
 
-void* firstList(List * list);
-void* nextList(List * list);
-void* lastList(List * list);
-void* prevList(List * list);
+void* firstList(List* list);
+void* nextList(List* list);
+void* lastList(List* list);
+void* prevList(List* list);
 
-void pushFront(List * list, void * data);
-void pushBack(List * list, void * data);
-void pushCurrent(List * list, void * data);
+void pushFront(List* list, void* data);
+void pushBack(List* list, void* data);
+void pushCurrent(List* list, void* data);
 
-void* popFront(List * list);
-void* popBack(List * list);
-void* popCurrent(List * list);
+void* popFront(List* list);
+void* popBack(List* list);
+void* popCurrent(List* list);
 
 #endif // List_h
 
