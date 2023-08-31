d4d49a3cf3151a9da225b7d6d5aaf80f09dfba2c
Estellar <142541831+MatiasPUCV@users.noreply.github.com>
2023-08-31 18:50:05 -0400

    Format on list.h

--- include/list.h
+++ include/list.h
@@ -3,30 +3,22 @@
 
 typedef struct List List;
 
-List * createList(void);
-
-void * firstList(List * list);
-
-void * nextList(List * list);
-
-void * lastList(List * list);
-
-void * prevList(List * list);
-
-void pushFront(List * list, void * data);
-
-void pushBack(List * list, void * data);
-
-void pushCurrent(List * list, void * data);
-
-void * popFront(List * list);
-
-void * popBack(List * list);
-
-void * popCurrent(List * list);
-
+List* createList(void);
 void cleanList(List * list);
 
-#endif /* List_h */
+void* firstList(List * list);
+void* nextList(List * list);
+void* lastList(List * list);
+void* prevList(List * list);
+
+void pushFront(List * list, void * data);
+void pushBack(List * list, void * data);
+void pushCurrent(List * list, void * data);
+
+void* popFront(List * list);
+void* popBack(List * list);
+void* popCurrent(List * list);
+
+#endif // List_h
 
 
