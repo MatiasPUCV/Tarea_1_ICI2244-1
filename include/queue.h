ca789529d372ce5baf1b1bb4dda070986c269cba
angelo8888 <142196931+angelo8888@users.noreply.github.com>
2023-09-01 21:18:38 -0400

    Add files via upload

---
+++ include/queue.h
@@ -0,0 +1,61 @@
+#ifndef Queue
+#define Queue
+
+typedef struct {
+    char name[50];
+} Student;
+
+typedef struct queueNode {
+    Student data;
+    struct queueNode* next;
+} queueNode;
+
+typedef struct {
+    queueNode* front;
+    queueNode* back;
+} Queue;
+
+Queue* createQueue() {
+    Queue* newQueue = (Queue*)malloc(sizeof(Queue));
+    newQueue->front = NULL;
+    newQueue->back = NULL;
+    return newQueue;
+}
+
+void push(Queue* queue, Student* data) {
+    queueNode* newNode = (queueNode*)malloc(sizeof(queueNode));
+    newNode->data = *data;
+    newNode->next = NULL;
+    
+    if (queue->back == NULL) {
+        queue->front = newNode;
+        queue->back = newNode;
+    } else {
+        queue->back->next = newNode;
+        queue->back = newNode;
+    }
+}
+
+void pop(Queue* queue) {
+    if (queue->front == NULL) {
+        return;
+    }
+    
+    queueNode* temp = queue->front;
+    queue->front = queue->front->next;
+    
+    if (queue->front == NULL) {
+        queue->back = NULL;
+    }
+    
+    free(temp);
+}
+
+Student* top(Queue* queue) {
+    if (queue->front == NULL) {
+        return NULL;
+    }
+    return &(queue->front->data);
+}
+
+#endif
